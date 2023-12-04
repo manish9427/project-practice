1052f7b47f3af0dbe85f1bdb4fed0ec0579e0b1c
manish9427 <manish119427@gmail.com>
2023-12-04 09:48:44 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
-import PageNotFound from "./pages/404/pageNotFound";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/pageNotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
