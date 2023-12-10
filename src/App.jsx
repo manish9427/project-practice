138600678a7e8d4fb87cd0f166963ad327092633
manish9427 <manish119427@gmail.com>
2023-12-10 11:21:44 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import "./App.css";
-import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
@@ -9,14 +7,18 @@ import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
 import PageNotFound from "./pages/404/pageNotFound";
 
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { fetchDataFromApi } from "./utils/api";
+
+import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
-import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   const dispatch = useDispatch();
   const { url } = useSelector((state) => state.home);
   console.log(url);
+
   useEffect(() => {
     fetchApiConfig();
   }, []);
@@ -24,7 +26,8 @@ function App() {
   const fetchApiConfig = () => {
     fetchDataFromApi("/configuration").then((res) => {
       console.log(res);
-      dispatch(getApiConfiguration(res));
+
+      dispatch(getApiConfiguration(url));
     });
   };
   return (
